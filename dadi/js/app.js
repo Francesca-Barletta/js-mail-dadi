e707420d1e9b90aa24f5d4d35f930a8201433741
Francesca-Barletta <wallebar@gmail.com>
2024-03-07 16:58:09 +0100

    add pseudocode and html contents in dadi

---
+++ dadi/js/app.js
@@ -0,0 +1,19 @@
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
+
+//devo generare due numeri random
+// devo recuperare il pulsante dal dom
+// devo associare l'evento click al pulsante, quindi la sua funzione:
+  //al click sul pulsante dell'utente,
+  //un numero sarà per l'utente e uno per il pc
+  //devo stampare nel dom i due numeri
+
+  //SE l'utente avrà il numero più alto 
+    //messaggio che ha vinto
+  // altrimenti 
+    //messaggio che ha perso
